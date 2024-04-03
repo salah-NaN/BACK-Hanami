afcf65339254f0bfec164789852f81e0da8bedb1
Joel128 <joelplazadi@gmail.com>
2024-04-03 23:26:07 +0200

    endpoints login&Register user&owner

--- routes/clientes.routes.js
+++ routes/clientes.routes.js
@@ -9,5 +9,7 @@ export default router
     .get("/clientes", async (req, res) => await readItems(req, res, Clientes))
     .get("/clientes/:id", async (req, res) => await readItem(req, res, Clientes))
     .post("/clientes", async (req, res) => await createItem(req, res, Clientes))
+    .post("/login", async (req, res) => await createItem(req, res, Clientes))
+    .post("/register", async (req, res) => await createItem(req, res, Clientes))
     .put("/clientes/:id", async (req, res) => await updateItem(req, res, Clientes))
     .delete("/clientes/:id", async (req, res) => await deleteItem(req, res, Clientes))
--- routes/propietarios.routes.js
+++ routes/propietarios.routes.js
@@ -9,5 +9,8 @@ export default router
     .get("/propietarios", async (req, res) => await readItems(req, res, Propietarios))
     .get("/propietarios/:id", async (req, res) => await readItem(req, res,Propietarios))
     .post("/propietarios", async (req, res) => await createItem(req, res, Propietarios))
+    .post("/propietarios/login", async (req, res) => await createItem(req, res, Propietarios))
+    .post("/propietarios/register", async (req, res) => await createItem(req, res, Propietarios))
+    // Si queremos que pueda modificar sus datos (email, contraseña, etc) .put("/propietarios/:id/login", async (req, res) => await updateItem(req, res, Propietarios))
     .put("/propietarios/:id", async (req, res) => await updateItem(req, res, Propietarios))
     .delete("/propietarios/:id", async (req, res) => await deleteItem(req, res, Propietarios))
