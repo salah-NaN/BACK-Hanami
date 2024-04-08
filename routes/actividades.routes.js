3c90d041da688f0c00e2594b1ae3508c12211d84
Joel Plaza Diaz <50558171+Joel128@users.noreply.github.com>
2024-04-08 18:56:37 +0200

    fix Actividades routes

--- routes/actividades.routes.js
+++ routes/actividades.routes.js
@@ -6,8 +6,8 @@ const router = Router();
 import Actividades from "../models/Actividades.js";
 
 export default router
-    .get("/actividades", checkToken, async (req, res) => await readItems(req, res, actividadesController) )
-    .get("/actividades/:id", checkToken, async (req, res) => await readItem(req, res, actividadesController))
-    .post("/actividades", checkToken, async (req, res) => await createItem(req, res, actividadesController))
-    .put("/actividades/:id", checkToken, async (req, res) => await updateItem(req, res, actividadesController))
-    .delete("/actividades/:id", checkToken, async (req, res) => await deleteItem(req, res, actividadesController));
+    .get("/actividades", checkToken, async (req, res) => await readItems(req, res, Actividades) )
+    .get("/actividades/:id", checkToken, async (req, res) => await readItem(req, res,Actividades ))
+    .post("/actividades", checkToken, async (req, res) => await createItem(req, res,Actividades ))
+    .put("/actividades/:id", checkToken, async (req, res) => await updateItem(req, res,Actividades))
+    .delete("/actividades/:id", checkToken, async (req, res) => await deleteItem(req, res,Actividades));
