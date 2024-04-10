61968512ce31ace5ec3a76a1eaca300ecc180560
Joel Plaza Diaz <50558171+Joel128@users.noreply.github.com>
2024-04-10 17:36:24 +0200

    Controllers Clientes

--- routes/clientes.routes.js
+++ routes/clientes.routes.js
@@ -1,3 +1,5 @@
+// imports controladores
+
 import {
   createItem,
   readItems,
@@ -5,19 +7,21 @@ import {
   updateItem,
   deleteItem,
 } from "../controllers/generics.controllers.js";
+import {register, login} from "../controllers/clientes.controllers.js";
+
+// imports dependencias
+
 import {Router} from "express";
 import Clientes from "../models/Clientes.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import checkToken from "../middleware/checkToken.js";
-import Resenias from "../models/Resenias.js";
 
 // constantes
 const router = Router();
 const secretKey = "esto_es_la_leche_de_creativo";
 
 // funciones añadidas
-
 // Al tener un register, he de hacer un beforeCreate para encriptar la
 // contraseña
 Clientes.beforeCreate(async (cliente) => {
@@ -51,46 +55,11 @@ export default router
     checkToken,
     async (req, res) => await deleteItem(req, res, Clientes)
   )
-
-  // register
-  .post("/clientes/register", async (req, res) => {
-    try {
-      const {nombre, email, password} = req.body;
-      if (!nombre || !email || !password) {
-        return res.status(400).json({message: "Todos los campos requeridos"});
-      }
-      const posibleCliente = await Clientes.findOne(
-        {where: {email}},
-        {raw: true}
-      );
-      if (posibleCliente)
-        return res.status(400).json({message: "Usuario registrado"});
-      const cliente = await Clientes.create(req.body);
-      if (!cliente) return res.status(404).json({message: "No encontrado"});
-      res.status(201).json(cliente);
-    } catch (error) {
-      res.status(400).json({error: error.message});
-    }
-  })
-
-  // login
-  .post("/clientes/login", async (req, res) => {
-    try {
-      const {email, password} = req.body;
-      const cliente = await Clientes.findOne({where: {email}});
-      if (!cliente) return res.status(404).json({error: "No encontrado"});
-      const verifyPassword = await bcrypt.compare(password, cliente.password);
-      // error contraseña inválida
-      if (!verifyPassword)
-        return res.status(401).json({error: "Contraseña inválida"});
-      const token = jwt.sign(
-        {cliente_id: cliente.id, cliente_nombre: cliente.nombre},
-        secretKey,
-        {expiresIn: "2h"}
-      );
-      res.cookie("token", token, {httpOnly: false, maxAge: 7200000});
-      res.json({cliente_id: cliente.id, cliente_nombre: cliente.nombre});
-    } catch (error) {
-      res.status(500).json({error: error.message});
-    }
-  });
+  .post(
+    "/clientes/register",
+    async (req, res) => await register(req, res, Clientes)
+  )
+  .post(
+    "/clientes/login",
+    async (req, res) => await login(req, res, Clientes, jwt, secretKey)
+  )
