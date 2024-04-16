9ca9b0d05c73bebbdad61f88b5337ddfad50c947
Salah Eddin <salah.extras@gmail.com>
2024-04-16 10:30:53 +0200

    enpoint creado de las actividades

--- controllers/actividades.controllers.js
+++ controllers/actividades.controllers.js
@@ -1,4 +1,5 @@
-import {Op, where} from "sequelize";
+
+import { Op, where } from "sequelize";
 
 const actividades_buscador = async (
   req,
@@ -13,7 +14,7 @@ const actividades_buscador = async (
     const flor = req.params.flor !== ";" ? req.params.flor : "%";
 
     const actividades_buscador = await Model.findAll({
-      where: {poblacion: {[Op.like]: poblacion}},
+      where: { poblacion: { [Op.like]: poblacion } },
       include: [
         {
           model: Temporadas,
@@ -22,12 +23,12 @@ const actividades_buscador = async (
             {
               model: PuntosInteres,
               required: true,
-              where: {id: {[Op.eq]: Temporadas.id}},
+              where: { id: { [Op.eq]: Temporadas.id } },
             },
           ],
           where: {
-            fecha_inicio: {[Op.lte]: fecha},
-            fecha_fin: {[Op.gte]: fecha},
+            fecha_inicio: { [Op.lte]: fecha },
+            fecha_fin: { [Op.gte]: fecha },
           },
         },
 
@@ -39,12 +40,59 @@ const actividades_buscador = async (
       ],
     });
     if (!actividades_buscador) {
-      return res.status(404).json({message: "No se encontraron actividades"});
+      return res.status(404).json({ message: "No se encontraron actividades" });
     }
     res.status(200).json(actividades_buscador);
   } catch (error) {
-    res.status(400).json({error: error.message});
+    res.status(400).json({ error: error.message });
   }
 };
 
-export {actividades_buscador};
+
+// endpoint dedicado a la página de una actividad específica
+const actividad_page = async (
+  req,
+  res,
+  Model,
+  Temporadas,
+  PuntosInteres,
+  Imagenes,
+  Resenias
+) => {
+  try {
+    const { id } = req.params
+    const actividad = await Model.findByPk(id, {
+      include: [
+        {
+          model: Temporadas,
+          include: [
+            {
+              model: PuntosInteres
+            }
+          ]
+        },
+        {
+          model: Resenias
+        }
+
+        // {
+        //   model: Imagenes
+        // }
+      ]
+    })
+
+    if (!actividad) {
+      return res.status(404).json({ message: "No se encontraron actividades" });
+    }
+    //codigo de prueba, es borrable
+    // const actividad_sola = await Model.findByPk(id)
+    // if (!actividad_sola) {
+    //   return res.status(404).json({ message: "No se encontraron actividades" });
+    // }
+    // const resenias = await actividad_sola.get
+    res.status(200).json(actividad);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+export { actividades_buscador, actividad_page };
