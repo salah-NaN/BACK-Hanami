8cbc8d0436747d90dbef28893bf8254c0416b68a
Manel-Gonzalez <manel.gonzalez.rico@gmail.com>
2024-04-19 14:13:06 +0200

    Actividades endpoint funciona

--- controllers/actividades.controllers.js
+++ controllers/actividades.controllers.js
@@ -1,34 +1,61 @@
-import { Op, where } from "sequelize";
+import {Op, where} from "sequelize";
 
 const actividades_buscador = async (
   req,
   res,
   Model,
   Temporadas,
+  Flores,
   PuntosInteres
 ) => {
   try {
     const poblacion = req.params.poblacion !== ";" ? req.params.poblacion : "%";
-    const fecha = req.params.fecha !== ";" ? req.params.fecha : new Date();
+    const fecha =
+      req.params.fecha !== ";" ? new Date(req.params.fecha) : new Date();
     const flor = req.params.flor !== ";" ? req.params.flor : "%";
 
+    const condicion = req.params.fecha;
+
+    //preparamos una const que es la fecha dentro de 1 año respecto el dia de hoy
+    const mas1Ano = new Date();
+    mas1Ano.setFullYear(mas1Ano.getFullYear() + 1);
+
     const actividades_buscador = await Model.findAll({
-      where: { poblacion: { [Op.like]: poblacion } },
+      where: {
+        poblacion: {[Op.like]: poblacion},
+      },
       include: [
         {
           model: Temporadas,
           required: true,
           include: [
             {
+              model: Flores,
+              required: true,
+              where: {
+                especie: {[Op.like]: flor},
+              },
+            },
+            {
               model: PuntosInteres,
               required: true,
-              where: { id: { [Op.eq]: Temporadas.id } },
             },
           ],
-          where: {
-            fecha_inicio: { [Op.lte]: fecha },
-            fecha_fin: { [Op.gte]: fecha },
-          },
+          where:
+            //miramos si lo que nos llega es un ";", que es si el usuario  ha introducido algun dato
+            condicion !== ";"
+              ? {
+                  [Op.and]: {
+                    fecha_inicio: {[Op.lte]: fecha},
+                    fecha_fin: {[Op.gte]: fecha},
+                  },
+                }
+              : {
+                  [Op.and]: {
+                    fecha_inicio: {[Op.gte]: fecha},
+                    fecha_fin: {[Op.lte]: mas1Ano},
+                  },
+                },
         },
 
         /*             {
@@ -39,11 +66,11 @@ const actividades_buscador = async (
       ],
     });
     if (!actividades_buscador) {
-      return res.status(404).json({ message: "No se encontraron actividades" });
+      return res.status(404).json({message: "No se encontraron actividades"});
     }
     res.status(200).json(actividades_buscador);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({error: error.message});
   }
 };
 
@@ -58,31 +85,31 @@ const actividad_page = async (
   Resenias
 ) => {
   try {
-    const { id } = req.params
+    const {id} = req.params;
     const actividad = await Model.findByPk(id, {
       include: [
         {
           model: Temporadas,
           include: [
             {
-              model: PuntosInteres
+              model: PuntosInteres,
             },
             {
-              model:Imagenes
-            }
-          ]
+              model: Imagenes,
+            },
+          ],
         },
         {
-          model: Resenias
+          model: Resenias,
         },
         {
-           model: Imagenes
-        }
-      ]
-    })
+          model: Imagenes,
+        },
+      ],
+    });
 
     if (!actividad) {
-      return res.status(404).json({ message: "No se encontraron actividades" });
+      return res.status(404).json({message: "No se encontraron actividades"});
     }
     //codigo de prueba, es borrable
     // const actividad_sola = await Model.findByPk(id)
@@ -92,8 +119,8 @@ const actividad_page = async (
     // const resenias = await actividad_sola.get
     res.status(200).json(actividad);
   } catch (error) {
-    res.status(400).json({ error: error.message });
+    res.status(400).json({error: error.message});
   }
-}
+};
 
 export {actividades_buscador, actividad_page};
